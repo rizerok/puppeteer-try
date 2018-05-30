230ba28d9d59f62142c1b416d60041001fb9c1cf
rizerok <bashinsky.alexey@gmail.com>
2018-05-30 20:00:39 +0300

    new commit

--- cmd/test.js
+++ cmd/test.js
@@ -1,11 +1,27 @@
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 
-const run = async () =>{
-  let obj = await exec('dir');
-  console.log('stdout:', obj.stdout);
-  obj = await exec('echo 456');
-  console.log('stdout:', obj.stdout);
+const neededBranch = 'master';
+
+const execWithConsole = async (command) => {
+  const {stdout, stderr} = await exec(command);
+  console.log(stdout, stderr);
+  return stdout || stderr;
+};
+
+
+const run = async () => {
+  //await execWithConsole('git fetch');
+  //await execWithConsole('git pull');
+  let branch = await execWithConsole('git branch');
+  branch = branch.match(/\*\s(\w+)/)[1];
+  console.log(`current branch - ${branch}`);
+  // if(branch !== neededBranch){
+  //   await execWithConsole(`git checkout ${neededBranch}`);
+  // }
+  await execWithConsole('git add .');
+  await execWithConsole('git commit -m "new commit"');
+  await execWithConsole('git push');
 };
 
 run();
